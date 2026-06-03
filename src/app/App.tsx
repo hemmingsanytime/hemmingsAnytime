f1d8380afa1b2e6be7263e2b0f5d8edd4fadd643
Kiran <kirankiran112005@gmail.com>
2026-06-03 17:58:10 +0530

    Feat: integrate Vercel Analytics into React application root

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import { RouterProvider } from 'react-router';
 import { router } from './routes';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      <RouterProvider router={router} />
+      <Analytics />
+    </>
+  );
 }
 
 export default App;
